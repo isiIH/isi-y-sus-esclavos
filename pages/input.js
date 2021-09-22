c85e7d18b55ccb25c894b192737d7b5a740f9e73
Luis Llanca <luis.llanca@alumnos.uach.cl>
2021-09-22 12:58:33 -0300

    ahora la arreglue

--- pages/input.js
+++ pages/input.js
@@ -1,62 +1,38 @@
-import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
-const Index = () => {
+import { useState } from "react";
+
+const Input = () => {
+  // Usando useState se puede manejar datos asociados al componente
+  // Como en este caso, los datos del input
+  const [value, setValue] = useState("");
+
   return (
-    <div className="container">
-      <Head>
-        <title>Calendario Academico</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <p>Input</p>
+      <input
+        value={value}
+        onChange={({ target: { value } }) => {
+          setValue(value);
+        }}
+      />
+      <button
+        disabled={value.length === 0}
+        onClick={async () => {
+          const response = await fetch("/api/hello", {
+            method: "POST",
+            body: value,
+          });
 
-      <main>
-        
+          // Abre una alerta en la ventana del navegador mostrando
+          // lo que la API respondio
+          window.alert(`Response: ${await response.text()}`);
 
-        <h1 className="title">2021</h1>
-
-        <p className="description">
-          Seleccione area de interes
-        </p>
-
-        <div className="grid">
-          <Link
-            href={{
-              pathname: "/response",
-              query: { opt: 0 },
-            }}
-          >
-            <a className="card">Becas</a>
-          </Link>
-          <Link
-            href={{
-              pathname: "/response",
-              query: { opt: 1 },
-            }}
-          >
-            <a className="card">Inscripciones</a>
-          </Link>
-          <Link
-            href={{
-              pathname: "/response",
-              query: { opt: 2 },
-            }}
-          >
-            <a className="card">Examenes</a>
-          </Link>
-        </div>
-      </main>
-
-      <footer>
-        <a className="xd" href="https://github.com/PabloSzx/INFO104-2021-1" target="_blank">
-          La isi hizo esto.
-        </a>
-        &nbsp;-&nbsp;
-        <Link href="/about">
-          <a className="xd">Derechos Reservados 2021.</a>
-        </Link>
-      </footer>
+          setValue("");
+        }}
+      >
+        Send input to API
+      </button>
     </div>
   );
 };
 
-export default Index;
+export default Input;
