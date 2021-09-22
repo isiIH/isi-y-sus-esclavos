34a0dd502aab286416160e938825bb781ffc9cc7
Luis Llanca <luis.llanca@alumnos.uach.cl>
2021-09-22 12:31:14 -0300

    xd

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => {
   return (
     <div className="container">
       <Head>
-        <title>INFO104 Nextjs App</title>
+        <title>xdxdxd</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
