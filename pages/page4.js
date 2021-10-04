248ded2ec9e5dfa0ab2a7f37507a8bb3e42c1a90
isidora hernández <isidora.hernandez@alumnos.uach.cl>
2021-10-04 15:23:12 -0300

    Edité un poco el timeline

--- pages/page4.js
+++ pages/page4.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Chrono } from "react-chrono";
 import data from "../public/data/data.js";
+import items from "../public/data/calendario.json"
 
 export default function App() {
     return (
         <div className="App">
+            <h1>Calendario Académico de Pregrado Uach.</h1>
             <div style={{ width: "100%", height: "720px" }}>
                 <Chrono
-                    items={data} 
+                    items={data}
+                    disableNavOnKey
                     slideShow
                     enableOutline
+                    cardHeight={100}
+                    cardWidth={500}
+                    useReadMore={false}
+                    borderLessCards
                     theme={{
                         primary: "black",
                         secondary: "orange",
-                        cardBgColor: "white",
+                        cardBgColor: "LightGray",
                         cardForeColor: "black",
                         titleColor: "black"
                     }}
--- public/data/data.js
+++ public/data/data.js
@@ -1,16 +1,18 @@
+import items from "../data/calendario.json"
+
+/*const data = [],
+
+for(const i=0; i<items.eventos.length; i++){
+    data.push({
+        
+    });
+}*/
+
 const data = [
     {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
-        cardDetailedText: "uwu",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "../../images/gato.png"
-            }
-        }
+        title: items.eventos[0].fechaInicio + " | " + items.eventos[0].fechaTermino,
+        cardTitle: items.eventos[0].titulo,
+        cardDetailedText: items.eventos[0].descripcion,
     },
     {
         title: "May 1940",
