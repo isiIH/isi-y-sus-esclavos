96ccd90d9e9ecb780399a158db6358b325885d7d
isidora hernández <isidora.hernandez@alumnos.uach.cl>
2021-10-04 15:50:14 -0300

    Agregué los datos del calendario al timeline

--- public/data/data.js
+++ public/data/data.js
@@ -1,58 +1,17 @@
 import items from "../data/calendario.json"
 
-/*const data = [],
+var data = [];
 
-for(const i=0; i<items.eventos.length; i++){
-    data.push({
-        
-    });
-}*/
+for(var i=0; i<items.eventos.length; i++){
+    data.push(
+        {
+            title: items.eventos[i].fechaInicio + " | " + items.eventos[i].fechaTermino,
+            cardTitle: items.eventos[i].titulo,
+            cardDetailedText: items.eventos[i].descripcion,
+        }
+    );
+}
 
-const data = [
-    {
-        title: items.eventos[0].fechaInicio + " | " + items.eventos[0].fechaTermino,
-        cardTitle: items.eventos[0].titulo,
-        cardDetailedText: items.eventos[0].descripcion,
-    },
-    {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
-        cardDetailedText: "uwu",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "../../images/gato.png"
-            }
-        }
-    },
-    {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
-        cardDetailedText: "uwu",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "../../images/gato.png"
-            }
-        }
-    },
-    {
-        title: "May 1940",
-        cardTitle: "Dunkirk",
-        cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
-        cardDetailedText: "uwu",
-        media: {
-            type: "IMAGE",
-            source: {
-                url: "../../images/gato.png"
-            }
-        }
-    }
-];
+console.log(items.eventos.length);
 
 export default data;
