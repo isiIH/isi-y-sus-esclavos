e31bcce9bc40f1771b4c9e8165ebf1f5fbbee7d7
Benja3o3 <anugamer303@gmail.com>
2021-10-06 13:37:20 -0300

    añadi un data2 para agregar los elementos al calendario

--- pages/about.js
+++ pages/about.js
@@ -1,37 +1,12 @@
 import Head from "next/head";
+import data from "../public/data/data2.js";
 import dynamic from "next/dynamic";
 const TimeLine = dynamic(() => import("react-gantt-timeline"), { ssr: false });
 
 const About = () => {
   let nombres = [];
-  let eventos = 20;
+  let datos = data;
 
-  for (let i = 0; i < eventos; i++) {
-    nombres[i] = i;
-  }
-
-  let d1 = new Date();
-  let d2 = new Date();
-  d2.setDate(d2.getDate() + 5);
-  let d3 = new Date();
-  d3.setDate(d3.getDate() + 8);
-  let d4 = new Date();
-  d4.setDate(d4.getDate() + 20);
-  let data = [
-    {
-      id: 1,
-      start: d1,
-      end: d2,
-      name: "Demo Task 1"
-    },
-    {
-      id: 2,
-      start: d3,
-      end: d4,
-      name: "Demo Task 2",
-      color: "orange"
-    }
-  ];
   return (
     <div className="container">
       <Head>
@@ -42,7 +17,7 @@ const About = () => {
       <main>
         <h1 className="title">Calendario Academico 2021</h1>
         <div className="time-line-container">
-          <TimeLine data={data} />
+          <TimeLine data={datos} />
         </div>
       </main>
     </div>
