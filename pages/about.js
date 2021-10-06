a11fc0c0c93d0c8f015a86fc69b4df550b5a48e4
Benja3o3 <anugamer303@gmail.com>
2021-10-06 12:45:55 -0300

    puse el timeline del calendario

--- pages/about.js
+++ pages/about.js
@@ -1,26 +1,50 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
+const TimeLine = dynamic(() => import("react-gantt-timeline"), { ssr: false });
 
 const About = () => {
+  let nombres = [];
+  let eventos = 20;
+
+  for (let i = 0; i < eventos; i++) {
+    nombres[i] = i;
+  }
+
+  let d1 = new Date();
+  let d2 = new Date();
+  d2.setDate(d2.getDate() + 5);
+  let d3 = new Date();
+  d3.setDate(d3.getDate() + 8);
+  let d4 = new Date();
+  d4.setDate(d4.getDate() + 20);
+  let data = [
+    {
+      id: 1,
+      start: d1,
+      end: d2,
+      name: "Demo Task 1"
+    },
+    {
+      id: 2,
+      start: d3,
+      end: d4,
+      name: "Demo Task 2",
+      color: "orange"
+    }
+  ];
   return (
     <div className="container">
       <Head>
-        <title>About</title>
+        <title>Calendario academico</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">Sobre esta página</h1>
-
-        <p className="description">
-          Este código está en <code>pages/about.js</code>
-        </p>
+        <h1 className="title">Calendario Academico 2021</h1>
+        <div className="time-line-container">
+          <TimeLine data={data} />
+        </div>
       </main>
-
-      <footer>
-        <a href="https://github.com/isiIH/isi-y-sus-esclavos" target="_blank">
-          Repositorio
-        </a>
-      </footer>
     </div>
   );
 };
