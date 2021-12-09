b496a87ad593878de0d82da60b8241e427eacdf1
isidora hernández <isidora.hernandez@alumnos.uach.cl>
2021-12-09 18:05:51 -0300

    Limpié el código x2

--- pages/creditos.js
+++ pages/creditos.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 const Creditos = () => {
     return (
-        <div className="contenedor-principal">
-        <Head>
-            <title>Calendario Académico</title>
-            <link rel="icon" href="/images/uach.png" />
-        </Head>
-        <main>
-            HOLA uwu
-        </main>
+        <div>
+            <Head>
+                <title>Calendario Académico</title>
+                <link rel="icon" href="/images/uach.png" />
+            </Head>
+            <main>
+                HOLA uwu
+            </main>
         </div>
     );
 };
--- public/data/calendario.js
+++ public/data/calendario.js
@@ -4,27 +4,27 @@ import dynamic from "next/dynamic";
 const TimeLine = dynamic(() => import("react-gantt-timeline"), { ssr: false });
 
 export function CalendV(f) {
-    return(
-        <>
-        <div className="time-line-container" style={{height: "700px"}}>
-            <Chrono 
-                items={dataV(f)} 
-                enableOutline
-                //slideShow
-                useReadMore={false}
-                cardHeight="100px"
-                theme={{ 
-                    primary: "black",
-                    secondary: "orange",
-                    cardBgColor: "#8fbbaf",
-                    cardForeColor: "black",
-                    titleColor: "black"
-                }}
-                mode="VERTICAL_ALTERNATING"
-            />
-        </div>
-        </>
-    );
+  return(
+    <>
+      <div className="time-line-container" id="vertical">
+        <Chrono 
+          items={dataV(f)} 
+          enableOutline
+          //slideShow
+          useReadMore={false}
+          cardHeight="100px"
+          theme={{ 
+            primary: "black",
+            secondary: "orange",
+            cardBgColor: "#8fbbaf",
+            cardForeColor: "black",
+            titleColor: "black"
+          }}
+          mode="VERTICAL_ALTERNATING"
+        />
+      </div>
+    </>
+  );
 }
 
 export function dataV(filtro) {
@@ -131,7 +131,7 @@ export function CalendH(data) {
   return(
     <>
       <p id="text-calend">Desplácese a la izquierda si no visualiza los eventos</p>
-      <div className="time-line-container">
+      <div className="time-line-container" id="horizontal">
           <TimeLine data={data} config={config}/>
       </div>
       
@@ -147,63 +147,57 @@ export function CalendH(data) {
 }
 
 export function dataH(filtro,bool) {
-    var data = [];
-    var hoy = new Date();
-    var dia = hoy.getDate();
-    var mes = hoy.getMonth()+1;
-    var año = hoy.getFullYear();
+  var data = [];
+  var hoy = new Date();
+  var dia = hoy.getDate();
+  var mes = hoy.getMonth()+1;
+  var año = hoy.getFullYear();
 
-    for (var i = 0; i < items.eventos.length; i++) {
-        if(filtro == "todos" || filtro == items.eventos[i].tipo || filtro == items.eventos[i].semestre){
-            if(items.eventos[i].fechaInicio != "?"){
-                var dia_inicio = items.eventos[i].fechaInicio[0] + items.eventos[i].fechaInicio[1];
-                var mes_inicio = items.eventos[i].fechaInicio[3] + items.eventos[i].fechaInicio[4];
-                var dia_fin = items.eventos[i].fechaTermino[0] + items.eventos[i].fechaTermino[1];
-                var mes_fin = items.eventos[i].fechaTermino[3] + items.eventos[i].fechaTermino[4];
-                var año_inicio = items.eventos[i].fechaInicio.substring(6,10);
-                var año_fin = items.eventos[i].fechaTermino.substring(6,10);
-            }
+  for (var i = 0; i < items.eventos.length; i++) {
+    if(filtro == "todos" || filtro == items.eventos[i].tipo || filtro == items.eventos[i].semestre){
+      if(items.eventos[i].fechaInicio != "?"){
+        var dia_inicio = items.eventos[i].fechaInicio[0] + items.eventos[i].fechaInicio[1];
+        var mes_inicio = items.eventos[i].fechaInicio[3] + items.eventos[i].fechaInicio[4];
+        var dia_fin = items.eventos[i].fechaTermino[0] + items.eventos[i].fechaTermino[1];
+        var mes_fin = items.eventos[i].fechaTermino[3] + items.eventos[i].fechaTermino[4];
+        var año_inicio = items.eventos[i].fechaInicio.substring(6,10);
+        var año_fin = items.eventos[i].fechaTermino.substring(6,10);
+      }
             
-            if (items.eventos[i].fechaTermino == "" || items.eventos[i].fechaInicio == "?") {
-                dia_fin = (parseInt(dia_inicio) + 1).toString();
-                mes_fin = mes_inicio;
-                año_fin = año_inicio;
-            } else if (items.eventos[i].fechaInicio == "") {
-                dia_inicio = (parseInt(dia_fin) - 1).toString();
-                mes_inicio = mes_fin;
-                año_inicio = año_fin;
-            }
+      if (items.eventos[i].fechaTermino == "" || items.eventos[i].fechaInicio == "?") {
+        dia_fin = (parseInt(dia_inicio) + 1).toString();
+        mes_fin = mes_inicio;
+        año_fin = año_inicio;
+      } else if (items.eventos[i].fechaInicio == "") {
+        dia_inicio = (parseInt(dia_fin) - 1).toString();
+        mes_inicio = mes_fin;
+        año_inicio = año_fin;
+      }
             
-            var j = 0;
-            while(items.eventos[i].tipo != items.leyenda[j].tipo){
-                j++;
-            }
+      var j = 0;
+      while(items.eventos[i].tipo != items.leyenda[j].tipo) j++;
 
-            var mostrar;
-            if(parseInt(año_fin) > año)
-              mostrar = true;
-            else 
-              if(parseInt(año_fin) == año && parseInt(mes_fin) > mes)
-                mostrar = true;
-              else 
-                if(parseInt(año_fin) == año && parseInt(mes_fin) == mes && parseInt(dia_fin) >= dia)
-                  mostrar = true;
-                else
-                  mostrar = false;
+      var mostrar;
+      if(parseInt(año_fin) > año) mostrar = true;
+      else 
+        if(parseInt(año_fin) == año && parseInt(mes_fin) > mes) mostrar = true;
+        else 
+          if(parseInt(año_fin) == año && parseInt(mes_fin) == mes && parseInt(dia_fin) >= dia) mostrar = true;
+          else mostrar = false;
 
-            if(bool || mostrar){
-              data.push(
-                  {
-                    id: i,
-                    name: items.eventos[i].titulo,
-                    start: new Date(año_inicio, mes_inicio - 1, dia_inicio),
-                    end: new Date(año_fin, mes_fin - 1, dia_fin),
-                    color: items.leyenda[j].color
-                  }
-              );
-            }
-        }
+      if(bool || mostrar){
+        data.push(
+          {
+            id: i,
+            name: items.eventos[i].titulo,
+            start: new Date(año_inicio, mes_inicio - 1, dia_inicio),
+            end: new Date(año_fin, mes_fin - 1, dia_fin),
+            color: items.leyenda[j].color
+          }
+        );
+      }
     }
+  }
     
-    return (data);
+  return (data);
 }
